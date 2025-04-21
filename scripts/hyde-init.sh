eabb1bdbb5298ff470caf484e9133654055d2280
Frank Junior <frankcbjunior@gmail.com>
2025-04-21 17:01:19 -0300

    update HyDE installation

--- scripts/hyde-init.sh
+++ scripts/hyde-init.sh
@@ -7,30 +7,20 @@ sudo pacman -S --needed --noconfirm openssh
 sudo systemctl start sshd
 sudo systemctl enable sshd
 
-
 # HyDE Install
 sudo pacman -S --needed --noconfirm git base-devel
 git clone --depth 1 https://github.com/HyDE-Project/HyDE ~/HyDE
 cd ~/HyDE/Scripts || return
 
-core_packages_file="pkg_core.lst"
-
 # Removing some packages
-sed -i 's/^eza|zsh/# &/' "$core_packages_file"
-sed -i 's/^eza|fish/# &/' "$core_packages_file"
-sed -i 's/^zsh-theme-powerlevel10k-git|zsh/# &/' "$core_packages_file"
-sed -i 's/^pokego-bin|zsh/# &/' "$core_packages_file"
-sed -i 's/^code/# &/' "$core_packages_file"
-sed -i 's/^firefox/# &/' "$core_packages_file"
+black_list_packages="pkg_black.lst"
+{
+  echo "eza"
+  echo "pokego-bin"
+  echo "code"
+  echo "firefox"
+  echo "zsh-theme-powerlevel10k-git"
+} >> "$black_list_packages"
 
 clear
 ./install.sh
-
-# Install more themes
-cd ~/HyDE/Scripts || return
-./themepatcher.sh "Greenify" https://github.com/mahaveergurjar/Theme-Gallery/tree/Greenify
-
-# Reboot
-echo "rebooting in 5 seconds"
-sleep 5
-reboot
