3cc7cee88166e7f97e682a113f990f82e75efd55
Frank Junior <frankcbjunior@gmail.com>
2025-05-04 19:26:40 -0300

    extract 'projects' function to a separated file

--- src/roles/dotfiles/files/functions.sh
+++ src/roles/dotfiles/files/functions.sh
@@ -130,23 +130,6 @@ tstart(){
     tmux start && tmux a -t "$(tmux ls -F "#{session_name}" | fzf --height 40% --prompt="Select a tmux session: ")"
 }
 
-projects(){
-  project_list=($(tmuxinator list | sed -n '2p' | sed 's/ \+/ /g'))
-
-  selected_project=$(printf "%s\n" "${project_list[@]}" | grep -v "generic" |
-      fzf --header-first --header="Projects" --height=30%
-  )
-
-  # Verifica se um projeto foi selecionado
-  if [ -n "$selected_project" ]; then
-    # Inicia o projeto selecionado
-    tmuxinator start "$selected_project"
-  else
-    echo "Nenhum projeto selecionado."
-    return 0
-  fi
-}
-
 tnew_session(){
     local session_name="$1"
     if [ -z "$session_name"  ];then
