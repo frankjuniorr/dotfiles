3ab8ff73d2625adfa9dc673290879d624f9ab064
Frank Junior <frankcbjunior@gmail.com>
2025-06-15 22:26:06 -0300

    change cursor in kitty and nvim

--- src/roles/neovim/files/lua/config/options.lua
+++ src/roles/neovim/files/lua/config/options.lua
@@ -5,3 +5,10 @@ vim.opt.winbar = "%=%m %f"
 
 -- yank command goes to clipboard too
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.guicursor = {
+	"n-v-c:block", -- bloco nos modos normal/visual/command
+	"i-ci-ve:ver25", -- i-beam (barra vertical) no modo insert
+	"r-cr:hor20", -- underline nos modos replace
+	"o:hor50", -- underline em operator-pending
+}
