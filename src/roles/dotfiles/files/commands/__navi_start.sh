bd7c712402489530958c8dab98c457e00ddcc8b4
Frank Junior <frankcbjunior@gmail.com>
2025-10-21 20:41:57 -0300

    add git cheat sheet

--- src/roles/dotfiles/files/commands/__navi_start.sh
+++ src/roles/dotfiles/files/commands/__navi_start.sh
@@ -1,8 +1,10 @@
 #!/bin/env bash
 
+current_dir=$(pwd)
 # Load FZF configs
 if [ -f ~/.config/dotfiles/__fzf_config.sh ]; then
   source ~/.config/dotfiles/__fzf_config.sh
 fi
 
+cd "$current_dir"
 navi
